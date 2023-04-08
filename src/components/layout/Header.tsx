2e43ebb3a9c9982b400c88220250a1f8fc35a6e9
Himanshu Rana <hr.himanshu143@gmail.com>
2023-04-08 12:10:58 +0530

    layout update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,26 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 
-import UnstyledLink from '@/components/atoms/links/UnstyledLink';
+import Link from "next/link";
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: "/", label: "Route 1" },
+  { href: "/", label: "Route 2" },
 ];
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
+    <header className="sticky top-0 z-50 bg-white">
+      <div className="layout flex h-14 items-center justify-between">
+        <Link href="/" className="font-bold hover:text-gray-600">
           Home
-        </UnstyledLink>
+        </Link>
         <nav>
-          <ul className='flex items-center justify-between space-x-4'>
+          <ul className="flex items-center justify-between space-x-4">
             {links.map(({ href, label }) => (
               <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
+                <Link href={href} className="hover:text-gray-600">
                   {label}
-                </UnstyledLink>
+                </Link>
               </li>
             ))}
           </ul>
@@ -29,3 +29,4 @@ export default function Header() {
     </header>
   );
 }
+
