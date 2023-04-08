d85405bb8a10776e70a5c65411bf802a6888101a
Himanshu Rana <hr.himanshu143@gmail.com>
2023-04-08 11:56:46 +0530

    update exten

--- src/components/blog/hero-post-old.js
+++ src/components/blog/hero-post-old.js
@@ -1,3 +1,6 @@
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+
 // import Avatar from "@/components/blog/avatar";
 // import CoverImage from "@/components/blog/cover-image";
 
